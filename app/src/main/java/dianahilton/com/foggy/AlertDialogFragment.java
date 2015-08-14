4f6fbaf2959bc8a0af4d0e8f170f3a30a04cfef1
Diana Hilton <diana.hilton@gmail.com>
2015-08-14 01:47:10 -0700

    Add AlertDialog Fragment, check if network exists. Add class for weather data.

---
+++ app/src/main/java/dianahilton/com/foggy/AlertDialogFragment.java
@@ -0,0 +1,25 @@
+package dianahilton.com.foggy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by Diana on 8/13/15.
+ */
+public class AlertDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
