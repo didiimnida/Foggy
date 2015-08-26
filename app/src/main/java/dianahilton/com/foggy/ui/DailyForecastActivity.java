1eb38b074f287a16900130b5b782c0baac50db76
Diana Hilton <diana.hilton@gmail.com>
2015-08-25 22:45:27 -0700

    Add custom adapter.

--- app/src/main/java/dianahilton/com/foggy/ui/DailyForecastActivity.java
+++ app/src/main/java/dianahilton/com/foggy/ui/DailyForecastActivity.java
@@ -2,24 +2,29 @@ package dianahilton.com.foggy.ui;
 
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 
 import dianahilton.com.foggy.R;
+import dianahilton.com.foggy.adapters.DayAdapter;
+import dianahilton.com.foggy.weather.Day;
 
 public class DailyForecastActivity extends ListActivity {
+
+    private Day[] mDays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
-        String[] daysOfTheWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        DayAdapter adapter = new DayAdapter(this, mDays);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                daysOfTheWeek);
-
-        setListAdapter(adapter);
+//        String[] daysOfTheWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1,
+//                daysOfTheWeek);
+//
+//        setListAdapter(adapter);
     }
 
 }
